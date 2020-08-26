060cbfc94f918680e342ce2528ed3e180f22b679
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2020-08-26 17:27:26 +0600

    Update WeaChecker.py

--- WeaChecker.py
+++ WeaChecker.py
@@ -9,16 +9,15 @@ eel.init('web')
 @eel.expose
 def get_weather(city):
     api_url = "http://api.openweathermap.org/data/2.5/weather"
-    if city == None:
-        return "Please enter a city"
-    else:
-        r = requests.post(url = api_url, params = {'q':city, 'APPID': YOUR_OPEN_WEATHER_MAP_API_KEY, 'units': 'metric'})
-        if r.status_code == 200:
-            response = json.loads(r.content)
-            temp = response['main']['temp']
-            desc = response['weather'][0]['description']
-        return "It's " + str(desc) + " in " + city.capitalize() + ", with a temperature of " + str(pytemperature.c2f(temp)) +"° Farenheit."
+    r = requests.post(url = api_url, params = {'q':city, 'APPID': YOUR_OPEN_WEATHER_MAP_API_KEY, 'units': 'metric'})
+    if r.status_code == 200:
+       response = json.loads(r.content)
+       temp = response['main']['temp']
+       desc = response['weather'][0]['description']
 
+       return "It's " + str(desc) + " in " + city.capitalize() + ", with a temperature of " + str(pytemperature.c2f(temp)) +"° Farenheit."
+    else:
+       return "Please enter a valid city"
 
 
 #options = {
@@ -26,5 +25,5 @@ def get_weather(city):
 #	'args': ['node_modules/electron/dist/electron.exe', '.']
 #}
 
-eel.start('index.html', size=(800, 600))
+eel.start('index.html', size=(1050, 600))
 #eel.start('index.html', port=3131, mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
